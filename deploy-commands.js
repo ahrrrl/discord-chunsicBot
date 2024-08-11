8e402f66661dd4040b1122a7d33071313f7e6ac7
mokoko <guswlso2@naver.com>
2024-08-11 18:27:37 +0900

    fix: deploy-commands 기존 커멘드 삭제

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,57 +1,38 @@
 const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-require('dotenv').config();
-
-const clientId = process.env.DISCORD_CLIENT_ID;
-// const guildId = process.env.DISCORD_GUILD_ID;
-const token = process.env.DISCORD_TOKEN;
+const { DISCORD_TOKEN, CLIENT_ID } = process.env;
+const fs = require('fs');
+const path = require('path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const commandFiles = fs
+  .readdirSync(path.join(__dirname, 'commands/utility'))
+  .filter((file) => file.endsWith('.js'));
 
-for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith('.js'));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
+for (const file of commandFiles) {
+  const command = require(`./commands/utility/${file}`);
+  commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
 
-// and deploy your commands!
 (async () => {
   try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
+    console.log('Started refreshing application (/) commands.');
+
+    // 기존 커맨드 삭제
+    const currentCommands = await rest.get(
+      Routes.applicationCommands(CLIENT_ID)
     );
 
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationCommands(clientId), {
-      body: commands,
-    });
+    for (const command of currentCommands) {
+      await rest.delete(Routes.applicationCommand(CLIENT_ID, command.id));
+    }
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
+    // 새로운 커맨드 등록
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
   }
 })();
