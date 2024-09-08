6b14b99e1278130bc9c78eddf8f576fdecd0bcd1
mokoko <guswlso2@naver.com>
2024-09-09 02:04:26 +0900

    feat: 길드 삭제시 관련 데이터 db에서 모두 삭제

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -11,10 +11,8 @@ const guildDelete = {
         `서버 ${guild.name} (ID: ${guild.id})가 데이터베이스에서 삭제되었습니다.`
       );
 
-      // 해당 서버와 관련된 다른 데이터(예: 일정, 알람 설정 등) 삭제
-      // 필요에 따라 다음과 같은 코드를 추가할 수 있습니다:
-      // await Schedule.deleteMany({ guildId: guild.id });
-      // await AlarmSetting.deleteMany({ guildId: guild.id });
+      await Schedule.deleteMany({ channelId: guild.id });
+      await AlarmSetting.deleteMany({ channelId: guild.id });
     } catch (error) {
       console.error(
         `서버 ${guild.name} (ID: ${guild.id}) 삭제 중 오류 발생:`,
